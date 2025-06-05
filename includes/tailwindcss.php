fd67c87edb48aef05145a03de3bf8e313e8967b4
Zagaz <andre_ranulfo@hotmail.com>
2025-06-05 10:34:05 -0300

    Optimize tailwindcss enqueue logic by directly calling filemtime in wp_enqueue_style

--- includes/tailwindcss.php
+++ includes/tailwindcss.php
@@ -10,12 +10,11 @@ add_action('wp_enqueue_scripts', 'wp_movies_enqueue_styles');
 add_action('wp_enqueue_scripts', function() {
     $css_file = get_template_directory() . '/dist/output.css';
     if (file_exists($css_file)) {
-        $mtime = filemtime($css_file);
         wp_enqueue_style(
             'tailwindcss',
             get_template_directory_uri() . '/dist/output.css',
             [],
-            $mtime
+            filemtime($css_file)
         );
     }
 });
