3e7767a07eb6a6d6846d872b151d399a8f1a5fe8
Zagaz <andre_ranulfo@hotmail.com>
2025-06-06 11:46:16 -0300

    Home page indentifier

--- home.php
+++ home.php
@@ -1,18 +1,12 @@
+<?php 
+// The Home Page
+?>
+
 <?php get_header(); ?>
 
 <?php //template part
 get_template_part('template-parts/home-upcoming-movies');
 get_template_part('template-parts/home-top-actors'); 
 ?>
-
-
-
-
-
-
-
-
-
-
 
 <?php get_footer(); ?>
